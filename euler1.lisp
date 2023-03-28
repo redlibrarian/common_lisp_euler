0e51a7670407214d4e9f770f0834fdfee6df1980
Sam Popowich <spopowich@gmail.com>
2023-03-27 19:37:12 -0500

    Update.

--- euler1.lisp
+++ euler1.lisp
@@ -6,8 +6,7 @@
   (remove-if-not #'multiples (loop for i from start below end collect i)))
 
 (defun euler1 (start end)
-  ;;Find the sum of all the multiples of 3 or 5 below 1000.
-  ;; syntax: (euler1 1 1000)
+  "Find the sum of all the multiples of 3 or 5 below 1000. Syntax: (euler1 1 1000)"
   (reduce #'+ (create-range start end)))
 
 
